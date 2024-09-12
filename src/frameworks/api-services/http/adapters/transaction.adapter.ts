87297adbd6611c294d1e534edbfabc265c5c2b0a
Jean Silva <jeans_bs@outlook.com>
2024-09-11 23:41:20 -0300

    test

--- src/frameworks/api-services/http/adapters/transaction.adapter.ts
+++ src/frameworks/api-services/http/adapters/transaction.adapter.ts
@@ -10,15 +10,13 @@ export class TransactionAdapter implements ITransactionPort {
     constructor(private readonly httpService: HttpService) { }
 
     getTransactionById(transactionId: string): Promise<AxiosResponse<Transaction>> {
-        const localURL = `http://0.0.0.0:3003/transactions/${transactionId}`;
-        const containerURL = `http://payment_ms:3003/transactions/${transactionId}`;
+        // const localURL = `http://0.0.0.0:3003/transactions/${transactionId}`;
+        // const containerURL = `http://payment_ms:3003/transactions/${transactionId}`;
+        
+        const finalUrl = `http://af8561f2d520f465a9b06c0f44283b36-47650767.us-east-1.elb.amazonaws.com/transactions/${transactionId}`;
 
         return this.httpService.
-            axiosRef.get(localURL)
-            .catch(() => {
-                return this.httpService.
-                    axiosRef.get(containerURL)
-            });
+            axiosRef.get(finalUrl);
     }
 
 }
